e0970e4a74789ba177d5cd09c070b439fb74083b
comefigo <comefigo@gmail.com>
2018-08-20 23:32:48 +0900

    update

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const express = require('express');
 // Constants
 const PORT = process.env.PORT ? process.env.PORT : 3000;
 const app_ver = process.env.VER ? process.env.VER : '0.0.1';
+const app_name = process.env.APP_NAME ? process.env.APP_NAME : 'myapp';
 
 // App
 const app = express();
 app.get('/', function (req, res) {
-    res.send('Hello world ver.' + app_ver + '\n');
+    res.send('Hello world (' + app_name + ') ver.' + app_ver + '\n');
 });
 
 app.listen(PORT);
